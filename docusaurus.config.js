8b31cf1ead2e2af9ee0c252290bdb239bd26816e
jovyinny <jovinemutelani@gmail.com>
2022-12-19 12:19:28 +0300

    fixed edit this page url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Neurotech-HQ/sarufi-docs',
+            'https://github.com/Neurotech-HQ/sarufi-docs/blob/main/',
         },
         blog: {
           showReadingTime: true,
