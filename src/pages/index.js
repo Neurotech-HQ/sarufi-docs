f4b0d29ecf8d3a0c5714feabf605907ebb001da4
Kalebu <isaackeinstein@gmail.com>
2022-12-28 15:26:59 +0300

    updated icon header

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Sarufi is a Conversational AI platform tailored to excel at African languages">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
